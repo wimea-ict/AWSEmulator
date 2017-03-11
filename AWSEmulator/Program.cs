e3f2e0c120aa0f71351cc06544229c5b6bb4d2a1
Maximus Byamukama <maximus.byamukama@gmail.com>
2017-03-12 00:16:30 +0300

    Add files via upload

---
+++ AWSEmulator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AWSEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter IP Address:\t");      //129.177.63.214 - bergen
+            //196.43.133.125 - MUK
+            string ipadd = Console.ReadLine();
+            Console.WriteLine("Enter Port:\t");
+            int port = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Delay between transmissions (in milli seconds):\t");
+            int ms = Int32.Parse(Console.ReadLine());
+
+            TcpClient client = new TcpClient(ipadd, port);
+            try
+            {
+                Stream s = client.GetStream();
+                Console.WriteLine("connected!");
+                StreamWriter sw = new StreamWriter(s);
+                StreamReader sr = new StreamReader("sample.dat");
+                sw.AutoFlush = true;
+                string line = "";
+                while ((line = sr.ReadLine())!=null)
+                {
+                        sw.WriteLine(line);
+                        System.Threading.Thread.Sleep(ms);
+                        Console.WriteLine("Data Sent: " + line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
